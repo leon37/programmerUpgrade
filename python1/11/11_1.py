24df2c12fb67df70afbe4ffb74b0e58edb01f326
leon <13368447@qq.com>
2022-11-24 00:12:21 +0800

    chapter 11 part 1

--- python1/11/11_1.py
+++ python1/11/11_1.py
@@ -4,3 +4,18 @@ project:programmerUpgrade
 date:2022/11/24
 email:13368447@qq.com
 '''
+import webbrowser
+import sys
+import pyperclip
+
+# webbrowser.open('http://inventwithpython.com')
+# 870 Valencia St, San Francisco, CA 94110
+
+address = ''
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+if address:
+    webbrowser.open('https://www.google.com/maps/place/{}'.format(address))
